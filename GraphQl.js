607a84164cbd625d556d302c1fc9aa922d127dc3
Anggelo <anstalyn1999@gmail.com>
2025-01-11 20:57:26 -0500

    feat: :green_heart: Add-GraphQl

---
+++ GraphQl.js
@@ -0,0 +1,27 @@
+const { ApolloServer, gql } = require('apollo-server');
+
+const typeDefs = gql`
+  type Product {
+    id: ID!
+    name: String!
+    price: Float!
+  }
+
+  type Query {
+    products: [Product]
+  }
+`;
+
+const products = [{ id: "1", name: "Apple", price: 0.5 }];
+
+const resolvers = {
+  Query: {
+    products: () => products,
+  },
+};
+
+const server = new ApolloServer({ typeDefs, resolvers });
+
+server.listen().then(({ url }) => {
+  console.log(`🚀 Server ready at ${url}`);
+});
